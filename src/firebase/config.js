1f7c2bec8f0767d533c2a6c3def934fba748efe3
sirtheprogrammer <sirtheprogrammer@gmail.com>
2025-10-24 22:27:03 +0300

    fix user output

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,27 +4,20 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 // Firebase configuration
+const firebaseConfig = {
   apiKey: "AIzaSyCeEfQ3_XCjN3YOdWUhFBLvyXyfUrDTVtk",
-
   authDomain: "anagroupsupplies.firebaseapp.com",
-
   projectId: "anagroupsupplies",
-
   storageBucket: "anagroupsupplies.firebasestorage.app",
-
   messagingSenderId: "203969283827",
-
   appId: "1:203969283827:web:09e5532f842eaff2b35986",
-
   measurementId: "G-93FN4Q8433"
-
 };
 
-
-// Initialize Firebase
+//  Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { app, analytics, auth, db }; 
+export { app, analytics, auth, db };
